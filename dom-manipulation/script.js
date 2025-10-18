53f2924c552f1ee89b6f01b36249907dfbdac263
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-10-18 15:48:09 +0100

    implement task 0

---
+++ dom-manipulation/script.js
@@ -0,0 +1,43 @@
+let quotes = [
+  {
+    text: "The only way to do great work is to love what you do.",
+    category: "Motivation",
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    category: "Life",
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    category: "Inspiration",
+  },
+];
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  const displayDiv = document.getElementById("quoteDisplay");
+  displayDiv.textContent = `"${quote.text}" - ${quote.category}`;
+}
+
+function addQuote() {
+  const newText = document.getElementById("newQuoteText").value.trim();
+  const newCategory = document.getElementById("newQuoteCategory").value.trim();
+
+  if (newText == "" || newCategory == "") {
+    alert("Please enter both a quote and a category!");
+    return;
+  }
+
+  quotes.push({ text: newText, category: newCategory });
+
+  document.getElementById("newQuoteText").value = "";
+  document.getElementById("newQuoteCategory").value = "";
+
+  alert("Quote added successfully!");
+
+  document
+    .getElementById("newQuote")
+    .addEventListener("click", showRandomQuote);
+  showRandomQuote();
+}
